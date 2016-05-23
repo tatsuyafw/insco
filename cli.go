5c05b78586ce6d16fba794a13d19774b053b682f
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2016-05-23 21:52:54 +0900

    [WIP] Create directories before an installation

--- cli.go
+++ cli.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/jessevdk/go-flags"
 	"io"
+	"os"
 )
 
 const (
@@ -11,18 +12,51 @@ const (
 	ExitCodeParserFlagError
 )
 
-func (cli *CLI) showHelp() {
-	// TODO: show usage
-	fmt.Fprintf(cli.errStream, "Usage: \n")
+const (
+	HomeEnv = "HOME"
+)
+
+type Options struct {
+	OptHelp    bool `short:"h" long:"help" description:"Show this help message and exit"`
+	OptVersion bool `short:"v" long:"version" description:"Print the version and exit"`
 }
 
 type CLI struct {
 	outStream, errStream io.Writer
 }
 
-type Options struct {
-	OptHelp    bool `short:"h" long:"help" description:"Show this help message and exit"`
-	OptVersion bool `short:"v" long:"version" description:"Print the version and exit"`
+func (cli *CLI) showHelp() {
+	// TODO: show usage
+	fmt.Fprintf(cli.errStream, "Usage: \n")
+}
+
+func homeDir() string {
+	homeDir := os.Getenv(HomeEnv)
+	// TODO: error if HOME environment variablie does not exist
+	return homeDir
+}
+
+func binaryDir() string {
+	return homeDir() + "/bin"
+}
+
+func prefixDir() string {
+	return homeDir() + "/usr/local"
+}
+
+func setup() error {
+	binaryDir := binaryDir()
+
+	if err := os.MkdirAll(binaryDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	prefixDir := prefixDir()
+	if err := os.MkdirAll(prefixDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (cli *CLI) parseOptions(args []string) (*Options, []string, error) {
@@ -58,7 +92,16 @@ func (cli *CLI) Run(args []string) int {
 		return ExitCodeParserFlagError
 	}
 
-	fmt.Println(parsedArgs)
+	target := parsedArgs[0]
+	// version := parsedArgs[1]
+
+	setup()
+
+	switch target {
+	case "emacs":
+	default:
+
+	}
 
 	return ExitCodeOK
 }
