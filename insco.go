e698d3745ded33cd14d9a5c0d56105b7e0fe42b6
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2016-06-05 21:53:17 +0900

    Delete the hello world message

--- insco.go
+++ insco.go
@@ -9,8 +9,6 @@ const Name string = "insco"
 const Version string = "0.0.1"
 
 func main() {
-	fmt.Println("Hello, insco!")
-
 	cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
 	os.Exit(cli.Run(os.Args[1:]))
 }
